72aa3819f3fc098c79f77a55a11aee1d8d193314
Mark Ulrich <71002222+BNS-MarkUlrich@users.noreply.github.com>
2024-03-21 17:19:17 +0100

    Added Input feature from @Minoord and implemented pointer
    Co-Authored-By: Minoord <70703952+Minoord@users.noreply.github.com>

---
+++ Assets/Scripts/Player/Interactor.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using Minoord.Input;
+using UnityEngine.InputSystem;
+using System;
+
+namespace Examen.Player
+{
+    [RequireComponent(typeof(Pointer))]
+    public class Interactor : MonoBehaviour
+    {
+        #region Testing
+        private void Start() => _pointer.OnPointedAtPosition += DebugPointer;
+        private void DebugPointer(Vector3 position) 
+            => Debug.DrawLine(transform.position, position, Color.red, 1f);
+        #endregion
+
+        private InputAction _interactAction;
+        private Pointer _pointer;
+
+        public Action OnInteract;
+
+        private void OnEnable() => InputManager.SubscribeToAction("Interact", OnInteractPerformed, out _interactAction);
+
+        private void OnDisable()
+        {
+            _interactAction.Disable();
+            _interactAction.performed -= OnInteractPerformed;
+        }
+
+        private void Awake() => _pointer = GetComponent<Pointer>();
+
+        private void OnInteractPerformed(InputAction.CallbackContext context)
+        {
+            _pointer.PointAtPosition();
+            OnInteract?.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Pointer.cs
@@ -0,0 +1,39 @@
+using System;
+using Minoord.Input;
+using UnityEngine;
+
+namespace Examen.Player
+{
+    public class Pointer : MonoBehaviour
+    {
+        [SerializeField] private LayerMask _pointerLayerMask;
+        private Camera _myCamera; // Replace with camera manager once this is implemented
+        private Vector3 _pointerWorldPosition;
+
+        public Action<Vector3> OnPointedAtPosition;
+
+        private void OnEnable() => InputManager.TryGetAction("PointerPosition").Enable();
+
+        private void OnDisable() => InputManager.TryGetAction("PointerPosition").Disable();
+
+        private void Awake()
+        {
+            if (TryGetComponent(out Camera camera))
+                _myCamera = camera;
+            else
+                _myCamera = Camera.main;
+        }
+
+        public void PointAtPosition()
+        {
+            Vector2 pointerPosition = InputManager.TryGetAction("PointerPosition").ReadValue<Vector2>();
+            
+            Ray pointerRay = _myCamera.ScreenPointToRay(pointerPosition);
+            if (Physics.Raycast(pointerRay, out RaycastHit hit, _pointerLayerMask))
+            {
+                _pointerWorldPosition = hit.point;
+                OnPointedAtPosition?.Invoke(_pointerWorldPosition);
+            }
+        }
+    }
+}
