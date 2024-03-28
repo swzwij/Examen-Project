f9c54649c0e579bfa979e98823cd964f107c4256
Minoord <70703952+Minoord@users.noreply.github.com>
2024-03-28 09:57:43 +0100

    Fixed rounding bug

--- Assets/Scripts/Spawning/ResourceSpawning/ResourceSpawner.cs
+++ Assets/Scripts/Spawning/ResourceSpawning/ResourceSpawner.cs
@@ -17,25 +17,33 @@ public class ResourceSpawner : MonoBehaviour
 
             for (int j = 0; j < area.spawnableResources.Count; j++)
             {
-                if (spawnPercentage <= 0)
+                if (spawnPercentage <= 0 || currentSpawnAmount <= 0)
                 {
                     Debug.LogError($"Can't spawn {area.spawnableResources[j].resource.name}, because you can't spawn more then 100% Resources");
                     break;
                 }
 
                 float percentage = area.spawnableResources[j].spawnChance;
-                spawnPercentage -= percentage;
 
-                if (spawnPercentage < 0)
+                if (spawnPercentage - percentage < 0)
                 {
                     Debug.LogWarning($"Percentage of {area.spawnableResources[j].resource.name} was to high so we rounded it down to {spawnPercentage}");
                     percentage = spawnPercentage;
+                    spawnPercentage = 0;
                 }
+                else
+                    spawnPercentage -= percentage;
 
-                float amountOfrescoures = MathF.Abs((float)area.AmountOfResourcesInTheArea / 100 * percentage);
+                float amountOfrescoures = MathF.Round((float)area.AmountOfResourcesInTheArea / 100 * percentage);
+                float newSpawnAmount = currentSpawnAmount - amountOfrescoures;
 
-                if (currentSpawnAmount - amountOfrescoures < 0)
+                if (newSpawnAmount < 0)
+                {
                     amountOfrescoures = currentSpawnAmount;
+                    currentSpawnAmount = 0;
+                }
+                else
+                    currentSpawnAmount = newSpawnAmount;
 
                 spawnTypes.Add(area.spawnableResources[j].resource, amountOfrescoures);
             }
