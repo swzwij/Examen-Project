7dac77058437bc2d0c2ef41386f7568c512f1f52
Mark Ulrich <71002222+BNS-MarkUlrich@users.noreply.github.com>
2024-04-08 10:53:29 +0200

    Added camera settings manager

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -5,12 +5,26 @@ namespace Examen.BrotherEye
 {
     public class CameraManager : MonoBehaviour
     {
-        private CameraTypes _currentCamera = CameraTypes.Player;
-        CameraSettings[] _cameraSettings;
-        Dictionary<CameraTypes, int> _cameraSettingsIndex = new()
+        [SerializeField] private CameraTypes _currentCameraType = CameraTypes.Player;
+        [SerializeField] private CameraSettings[] _cameraSettings;
+        private Dictionary<CameraTypes, int> _cameraSettingsIndex = new()
         {
             {CameraTypes.Player, 0},
             {CameraTypes.Ballistae, 1}
         };
+
+        public CameraTypes CurrentCameraType => _currentCameraType;
+        public CameraSettings CurrentCameraSettings => _cameraSettings[_cameraSettingsIndex[_currentCameraType]];
+
+        private void OnEnable() => SetCameraType(_currentCameraType);
+
+        public void SetCameraType(CameraTypes cameraType)
+        {
+            if (_cameraSettingsIndex.TryGetValue(cameraType, out int index))
+            {
+                _currentCameraType = cameraType;
+                _currentCameraSettings = _cameraSettings[index];
+            }
+        }
     }
 }
