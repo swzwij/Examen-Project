79778ff962eb412d97a2ac46d9a3b19cd52815a5
RyanDefost <70955105+RyanDefost@users.noreply.github.com>
2024-04-22 11:49:00 +0200

    speedRoad functional code

--- Assets/SpeedRoadStructure.cs
+++ Assets/SpeedRoadStructure.cs
@@ -1,18 +1,50 @@
-using System.Collections;
+using Examen.Pathfinding;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpeedRoadStructure : MonoBehaviour
+namespace Examen.Structure 
 {
-    // Start is called before the first frame update
-    void Start()
+    public class SpeedRoadStructure : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private float _speedMultiplier;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private Dictionary<GameObject, PathFollower> _players = new();
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.layer != 3) //Player layer
+                return;
+
+            PathFollower currentFollower = GetPathFollower(other.gameObject);
+
+            SetMultipliedSpeed(currentFollower, out float speed);
+            SetPathFollowerSpeed(currentFollower, speed);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.layer != 3) //Player layer
+                return;
+
+            PathFollower currentFollower = GetPathFollower(other.gameObject);
+
+            SetPathFollowerSpeed(currentFollower, currentFollower._baseSpeed);
+        }
+
+        private PathFollower GetPathFollower(GameObject currentObject)
+        {
+            if (!_players.ContainsKey(currentObject))
+                _players.Add(currentObject, currentObject.GetComponent<PathFollower>());
+
+            _players.TryGetValue(currentObject, out PathFollower currentFollower);
+
+            return currentFollower;
+        }
+
+        private float SetMultipliedSpeed(PathFollower pathFollower, out float speed) 
+            => speed = pathFollower._baseSpeed * _speedMultiplier;
+
+        private void SetPathFollowerSpeed(PathFollower pathFollower, float speed) 
+            => pathFollower.Speed = speed;
     }
 }
