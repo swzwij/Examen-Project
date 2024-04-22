f659818508019938f187839b159f478f39cb65e1
RyanDefost <70955105+RyanDefost@users.noreply.github.com>
2024-04-22 15:45:39 +0200

    reworked the collision detection

--- Assets/SpeedRoadStructure.cs
+++ Assets/SpeedRoadStructure.cs
@@ -1,47 +1,86 @@
 using Examen.Pathfinding;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Examen.Structure 
 {
     public class SpeedRoadStructure : MonoBehaviour
     {
-        [SerializeField] private float _speedMultiplier;
+        [SerializeField] private float _speedMultiplier = 1.5f;
 
-        private Dictionary<GameObject, PathFollower> _players = new();
+        private Dictionary<Collider, PathFollower> _players = new();
 
-        private void OnTriggerEnter(Collider other)
+        private Collider[] _overlappingColliders = new Collider[16];
+        private Collider[] _previousCollides = new Collider[16];
+
+        private void Update()
         {
-            if (other.gameObject.layer != 3) //Player layer
+            Physics.OverlapBoxNonAlloc(gameObject.transform.position, new Vector3(1.5f, 1, 3.5f), _overlappingColliders);
+            foreach (var collider in _overlappingColliders)
+            {
+                //Enter
+                if (!_players.ContainsKey(collider))
+                    EnterCollider(collider);
+
+                //Exit
+                else if (_previousCollides.Length < _overlappingColliders.Length)
+                    CheckForExits();
+            }
+
+            _previousCollides = _overlappingColliders;
+        }
+
+        private void CheckForExits()
+        {
+            foreach (var player in _players)
+            {
+                if (!_overlappingColliders.Contains(player.Key))
+                {
+                    _players.Remove(player.Key);
+                    ExitCollider(player.Key);
+
+                    break;
+                }
+            }
+        }
+
+        private void EnterCollider(Collider collider)
+        {
+            if (collider.gameObject.layer != 3) //Player layer
                 return;
 
-            PathFollower currentFollower = GetPathFollower(other.gameObject);
+            Debug.Log("ENTER COLLIDER");
 
-            SetMultipliedSpeed(currentFollower, out float speed);
+            PathFollower currentFollower = GetPathFollower(collider);
+
+            MultiplySpeed(currentFollower, out float speed);
             SetPathFollowerSpeed(currentFollower, speed);
         }
 
-        private void OnTriggerExit(Collider other)
+        private void ExitCollider(Collider collider)
         {
-            if (other.gameObject.layer != 3) //Player layer
+            if (collider.gameObject.layer != 3) //Player layer
                 return;
 
-            PathFollower currentFollower = GetPathFollower(other.gameObject);
+            Debug.Log("EXIT COLLIDER");
+
+            PathFollower currentFollower = GetPathFollower(collider);
 
             SetPathFollowerSpeed(currentFollower, currentFollower._baseSpeed);
         }
 
-        private PathFollower GetPathFollower(GameObject currentObject)
+        private PathFollower GetPathFollower(Collider currentCollider)
         {
-            if (!_players.ContainsKey(currentObject))
-                _players.Add(currentObject, currentObject.GetComponent<PathFollower>());
+            if (!_players.ContainsKey(currentCollider))
+                _players.Add(currentCollider, currentCollider.GetComponent<PathFollower>());
 
-            _players.TryGetValue(currentObject, out PathFollower currentFollower);
+            _players.TryGetValue(currentCollider, out PathFollower currentFollower);
 
             return currentFollower;
         }
 
-        private float SetMultipliedSpeed(PathFollower pathFollower, out float speed) 
+        private float MultiplySpeed(PathFollower pathFollower, out float speed) 
             => speed = pathFollower._baseSpeed * _speedMultiplier;
 
         private void SetPathFollowerSpeed(PathFollower pathFollower, float speed) 
