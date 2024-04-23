e016b25e711b53935c93647c66b76ae359d3aa49
Minoord <70703952+Minoord@users.noreply.github.com>
2024-04-23 15:44:16 +0200

    Server send to each player
    Co-Authored-By: Mark Ulrich <71002222+BNS-MarkUlrich@users.noreply.github.com>

--- Assets/Scripts/Spawning/BossSpawning/BossSpawningManager.cs
+++ Assets/Scripts/Spawning/BossSpawning/BossSpawningManager.cs
@@ -1,7 +1,11 @@
+using Examen.Networking;
 using Examen.Poolsystem;
+using FishNet.Component.Spawning;
+using FishNet.Connection;
 using FishNet.Object;
 using MarkUlrich.Health;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BossSpawningManager : NetworkBehaviour
@@ -9,13 +13,23 @@ public class BossSpawningManager : NetworkBehaviour
     [Header("Boss Spawning")]
     [SerializeField] private int _bossDownTimer = 60;
     [SerializeField] private HealthData _bossPrefab;
+    [SerializeField] private PlayerSpawner _spawner;
 
     [Header("Boss UI")]
     [SerializeField] private List<BossHealthBar> _sliders;
 
+    private int _spawnedBosses;
     private Dictionary<GameObject, BossHealthBar> _currentActiveBossSliders = new();
 
-    private void Start() => ServerInstance.Instance.OnServerStarted += CreateBoss;
+    private void Start()
+    {
+        ServerInstance.Instance.OnServerStarted += CreateBoss;
+
+        _spawner.OnSpawned += (NetworkObject obj) =>
+        {
+            SendBossInfo(obj.ClientManager.Connection);
+        };
+    }
 
     [Server]
     private void CreateBoss()
@@ -24,10 +38,24 @@ public class BossSpawningManager : NetworkBehaviour
 
         PoolSystem.Instance.AddActiveObject(healthData.name, healthData.gameObject);
         healthData.onDie.AddListener(() => StartNextSpawnTimer(healthData.gameObject));
+        _spawnedBosses++;
 
         AddSlider(healthData);
     }
 
+    private void SendBossInfo(NetworkConnection target)
+    {
+        ReceiveBossInfo(target, _currentActiveBossSliders);
+    }
+
+    [ObserversRpc]
+    private void ReceiveBossInfo(NetworkConnection connection, Dictionary<GameObject, BossHealthBar> currentActiveBossSliders)
+    {
+        foreach (var sliders in currentActiveBossSliders)
+        {
+            sliders.Value.gameObject.SetActive(true);
+        }
+    }
 
     private void AddSlider(HealthData bossHealth)
     {
@@ -40,15 +68,15 @@ public class BossSpawningManager : NetworkBehaviour
         BossHealthBar healthBar = _sliders[_currentActiveBossSliders.Count + 1];
 
         healthBar.BossHealthData = bossHealth;
-        healthBar.HealthSlider.enabled = true;
-        _currentActiveBossSliders.Add(bossHealth.gameObject, _sliders[_currentActiveBossSliders.Count + 1]);    
+        healthBar.gameObject.SetActive(true);
+        _currentActiveBossSliders.Add(bossHealth.gameObject, _sliders[_currentActiveBossSliders.Count + 1]);
     }
 
     private void StartNextSpawnTimer(GameObject bossObject)
     {
-        if(_currentActiveBossSliders.TryGetValue(bossObject, out BossHealthBar healthBar))
+        if (_currentActiveBossSliders.TryGetValue(bossObject, out BossHealthBar healthBar))
         {
-            healthBar.HealthSlider.enabled = false;
+            healthBar.gameObject.SetActive(true);
             _currentActiveBossSliders.Remove(bossObject);
         }
 
