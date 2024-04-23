2daf760e9586426bc0cc5158973be279c72b4906
Minoord <70703952+Minoord@users.noreply.github.com>
2024-04-23 09:58:40 +0200

    The players now see a update HP bar

--- Assets/Scripts/UI/BossHealthBar.cs
+++ Assets/Scripts/UI/BossHealthBar.cs
@@ -11,31 +11,31 @@ public class BossHealthBar : NetworkBehaviour
 
     [SerializeField] private Slider _healthBar;
 
-    private void Start() => ServerInstance.Instance.OnServerStarted += InitializeHealthBar;
+    private void Start()
+    {
+        if(IsServer)
+            ServerInstance.Instance.OnServerStarted += ServerInitialize;
+        else
+            ClientInitialize();
+    }
 
     private void Update()
     {
         if (_damage) _bossHealthData.TakeDamage(1);
     }
-    private void InitializeHealthBar()
-    {
-        if (IsClient)
-        {
-            _healthBar = GetComponent<Slider>();
-            _healthBar.maxValue = _bossHealthData.MaxHealth;
-        }
-        else
-        {
-            _bossHealthData.onDamageTaken.AddListener(CallSetHealth);
-        }
-    }
 
+    [Server]
+    private void ServerInitialize() => _bossHealthData.onDamageTaken.AddListener(CallSetHealth);
+
+     private void ClientInitialize()
+     {
+        _healthBar = GetComponent<Slider>();
+        _healthBar.maxValue = _bossHealthData.MaxHealth;
+        _healthBar.value = _healthBar.maxValue;
+     }
 
     private void CallSetHealth() => SetUIHealth(_bossHealthData.Health);
 
     [ObserversRpc]
-    private void SetUIHealth(float bossHealth)
-    {
-        _healthBar.value = bossHealth;
-    }
+    private void SetUIHealth(float bossHealth) => _healthBar.value = bossHealth;
 }
