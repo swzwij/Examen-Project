a4b8aa6dd791ec351354a5d4509f17784cc47eaf
Samuel <70766878+swzwij@users.noreply.github.com>
2024-04-25 14:39:05 +0200

    removed un used usings

--- Assets/Scripts/Pathfinding/Grid/GridSystem.cs
+++ Assets/Scripts/Pathfinding/Grid/GridSystem.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using MarkUlrich.Utils;
 using UnityEngine;
-using UnityEngine.Profiling;
 
 namespace Examen.Pathfinding.Grid
 {
--- Assets/Scripts/Pathfinding/Grid/Node.cs
+++ Assets/Scripts/Pathfinding/Grid/Node.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Profiling;
 
 namespace Examen.Pathfinding.Grid
 {
