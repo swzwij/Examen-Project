fb51aa3ac194f53ef230f0f70f6edb7cd7ba2094
Samuel <70766878+swzwij@users.noreply.github.com>
2024-05-08 10:59:48 +0200

    added build ui

--- Assets/Scripts/Items/building/Building UI/BuildMenuToggle.cs
+++ Assets/Scripts/Items/building/Building UI/BuildMenuToggle.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,27 +9,10 @@ namespace Examen.Building.BuildingUI
         [SerializeField] private BuildMenu _buildMenu;
         [SerializeField] private GameObject _hotBar;
 
-        private Button _toggle;
-
-        private void Awake()
+        public void ToggleBuildMenu(bool isActive)
         {
-            _toggle = GetComponent<Button>();
-        }
-
-        private void OnEnable()
-        {
-            _toggle.onClick.AddListener(ToggleMenu);
-        }
-
-        private void OnDisable()
-        {
-            _toggle.onClick.RemoveListener(ToggleMenu);
-        }
-
-        private void ToggleMenu()
-        {
-            _buildMenu.gameObject.SetActive(!_buildMenu.gameObject.activeSelf);
-            _hotBar.SetActive(!_hotBar.activeSelf);
+            _buildMenu.gameObject.SetActive(isActive);
+            _hotBar.SetActive(!isActive);
         }
     }
 }
---
+++ Assets/Scripts/Items/building/Building UI/CategorySelect.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CategorySelect : MonoBehaviour
+{
+    [SerializeField] private Button _offensiveButton;
+    [SerializeField] private Button _defensiveButton;
+
+    [SerializeField] private Sprite _offensiveSelectedSprite;
+    [SerializeField] private Sprite _offensiveUnselectedSprite;
+    [SerializeField] private Sprite _defensiveSelectedSprite;
+    [SerializeField] private Sprite _defensiveUnselectedSprite;
+
+    private void Start()
+    {
+        _offensiveButton.image.sprite = _offensiveSelectedSprite;
+        _defensiveButton.image.sprite = _defensiveUnselectedSprite;
+    }
+
+    private void OnEnable()
+    {
+        _offensiveButton.onClick.AddListener(OnOffensiveButtonClicked);
+        _defensiveButton.onClick.AddListener(OnDefensiveButtonClicked);
+    }
+
+    private void OnDisable()
+    {
+        _offensiveButton.onClick.RemoveListener(OnOffensiveButtonClicked);
+        _defensiveButton.onClick.RemoveListener(OnDefensiveButtonClicked);
+    }
+
+    private void OnOffensiveButtonClicked()
+    {
+        _offensiveButton.image.sprite = _offensiveSelectedSprite;
+        _defensiveButton.image.sprite = _defensiveUnselectedSprite;
+    }
+
+    private void OnDefensiveButtonClicked()
+    {
+        _offensiveButton.image.sprite = _offensiveUnselectedSprite;
+        _defensiveButton.image.sprite = _defensiveSelectedSprite;
+    }
+}
