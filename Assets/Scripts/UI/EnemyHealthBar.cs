fa298790fd09b6929347373c32b8501760f758bc
Minoord <70703952+Minoord@users.noreply.github.com>
2024-05-13 10:08:11 +0200

    Implemented code feedback

---
+++ Assets/Scripts/UI/EnemyHealthBar.cs
@@ -0,0 +1,39 @@
+using FishNet.Object;
+using MarkUlrich.Health;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Examen.UI
+{
+    public class EnemyHealthBar : NetworkBehaviour
+    {
+        [SerializeField] private bool _damage;
+        [SerializeField] private Slider _healthBar;
+
+        public HealthData EnemyHealthData { get; set; }
+        public Slider HealthSlider => _healthBar;
+
+        private void Update()
+        {
+            if (!IsServer)
+                _healthBar.transform.parent.rotation = Quaternion.LookRotation(Vector3.back);
+
+            if (_damage)
+                EnemyHealthData.TakeDamage(1);
+        }
+
+        public void ServerInitialize() => EnemyHealthData.onDamageTaken.AddListener(CallSetHealth);
+
+        public void ClientInitialize(float enemyMaxHealth)
+        {
+            _healthBar.maxValue = enemyMaxHealth;
+            _healthBar.value = _healthBar.maxValue;
+        }
+
+        private void CallSetHealth() => SetUIHealth(EnemyHealthData.Health);
+
+        [ObserversRpc]
+        private void SetUIHealth(float enemyHealth) => _healthBar.value = enemyHealth;
+
+    }
+}
