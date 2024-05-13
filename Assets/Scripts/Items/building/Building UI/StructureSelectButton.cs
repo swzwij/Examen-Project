be48e735430c105b91947288c307e1dd36dedbdb
Minoord <70703952+Minoord@users.noreply.github.com>
2024-05-13 15:44:00 +0200

    Tried make button not interactable

--- Assets/Scripts/Items/building/Building UI/StructureSelectButton.cs
+++ Assets/Scripts/Items/building/Building UI/StructureSelectButton.cs
@@ -1,4 +1,8 @@
+using Examen.Inventory;
+using Examen.Items;
 using FishNet.Object;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -10,12 +14,40 @@ namespace Examen.Building.BuildingUI
     {
         [SerializeField] protected GameObject _structurePreview;
         [SerializeField] protected NetworkObject _structure;
+        [SerializeField] protected List<StructureCost> _structureCost;
 
+        private Button _button;
         private BuildingManager _buildingManager;
 
-        private void Awake() => _buildingManager = GetComponentInParent<BuildingManager>();
+        private void Awake()
+        {
+            _buildingManager = GetComponentInParent<BuildingManager>();
+            _button = GetComponent<Button>();
+
+            InventorySystem.Instance.OnItemsChanged += PlaceStructure;
+        }
+
+        private void PlaceStructure(Dictionary<ItemInstance, int> _)
+        {
+            int _currentItems = 0;
+
+            foreach (StructureCost item in _structureCost)
+            {
+                if (InventorySystem.Instance.CurrentItems.ContainsKey(item.Item) && InventorySystem.Instance.CurrentItems[item.Item] > item.Amount)
+                    _currentItems++;
+            }
+
+            _button.interactable = _currentItems >= _structureCost.Count;
+        }
 
         public void OnPointerDown(PointerEventData eventData)
             => _buildingManager.SpawnStructurePreview(_structurePreview, _structure);
+    }
+
+    [Serializable]
+    public struct StructureCost
+    {
+        public ItemInstance Item;
+        public int Amount;
     }
 }
