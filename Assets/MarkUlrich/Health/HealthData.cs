65743b99d8888a520031bbc1694c1c9a3655d888
RyanDefost <70955105+RyanDefost@users.noreply.github.com>
2024-05-21 13:37:34 +0200

    added  broadcast functions

--- Assets/MarkUlrich/Health/HealthData.cs
+++ Assets/MarkUlrich/Health/HealthData.cs
@@ -1,13 +1,14 @@
+using FishNet.Object;
 using UnityEngine;
 using UnityEngine.Events;
 
 namespace MarkUlrich.Health
 {
-    public class HealthData : MonoBehaviour
+    public class HealthData : NetworkBehaviour
     {
         [SerializeField] private float health = 100f;
         private float _maxHealth;
-        
+
         public UnityEvent<HealthEvent> onHealthChanged = new UnityEvent<HealthEvent>();
         public UnityEvent onHealthAdded = new UnityEvent();
         public UnityEvent onDamageTaken = new UnityEvent();
@@ -17,7 +18,7 @@ namespace MarkUlrich.Health
         public float Health => health;
         public float MaxHealth => _maxHealth;
         public bool HasMaxHealth => health >= _maxHealth;
-        
+
         private bool _isHit;
         [HideInInspector] public bool isDead;
 
@@ -59,8 +60,18 @@ namespace MarkUlrich.Health
             isDead = false;
             AddHealth(newHealth);
             onResurrected?.Invoke();
-            
+
             TriggerChangedEvent(HealthEventTypes.Resurrect, newHealth);
+
+            BroadcastResurrect(newHealth);
+        }
+
+        private void BroadcastResurrect(float newHealth)
+        {
+            if (!IsOwner)
+                return;
+
+            Resurrect(newHealth);
         }
 
         public void TakeDamage(float damage)
@@ -72,8 +83,19 @@ namespace MarkUlrich.Health
             onDamageTaken?.Invoke();
             TriggerChangedEvent(HealthEventTypes.TakeDamage, damage);
             _isHit = false;
-            
+
+            BroadcastTakeDamage(damage);
+
             if (health <= 0) Die();
+        }
+
+        [ObserversRpc]
+        private void BroadcastTakeDamage(float damage)
+        {
+            if (!IsOwner)
+                return;
+
+            TakeDamage(damage);
         }
 
         private void Die()
@@ -82,6 +104,17 @@ namespace MarkUlrich.Health
             isDead = true;
             onDie?.Invoke();
             TriggerChangedEvent(HealthEventTypes.Die);
+
+            BroadcastDie();
+        }
+
+        [ObserversRpc]
+        private void BroadcastDie()
+        {
+            if (!IsOwner)
+                return;
+
+            Die();
         }
 
         public void Kill()
@@ -92,6 +125,16 @@ namespace MarkUlrich.Health
         public void DestroySelf()
         {
             Destroy(gameObject);
+            BroadcastDestroySelf();
+        }
+
+        [ObserversRpc]
+        private void BroadcastDestroySelf()
+        {
+            if (!IsOwner)
+                return;
+
+            DestroySelf();
         }
     }
 }
