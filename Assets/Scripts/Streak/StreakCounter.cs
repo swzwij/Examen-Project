d2c9b7f4e295440033f2da1d173c7b326c7a4eec
Samuel <70766878+swzwij@users.noreply.github.com>
2024-05-23 14:07:32 +0200

    Added event listener

--- Assets/Scripts/Streak/StreakCounter.cs
+++ Assets/Scripts/Streak/StreakCounter.cs
@@ -1,4 +1,5 @@
 using System;
+using Examen.Spawning.BossSpawning;
 using FishNet.Object;
 using MarkUlrich.Utils;
 
@@ -6,17 +7,6 @@ namespace Examen.Streak
 {
     public class StreakCounter : NetworkedSingletonInstance<StreakCounter>
     {
-        public bool plus;
-
-        private void Update()
-        {
-            if (plus)
-            {
-                IncreaseStreak();
-                plus = false;
-            }
-        }
-
         private int _currentStreak;
 
         public int CurrentStreak => _currentStreak;
@@ -26,14 +16,14 @@ namespace Examen.Streak
         protected override void OnEnable()
         {
             base.OnEnable();
-            // Listen to boss defeated event
+            EnemySpawner.Instance.OnEnemyDefeated += IncreaseStreak;
         }
 
-        private void OnDisable()
-        {
-            // Unlisten to boss defeated event
-        }
+        private void OnDisable() => EnemySpawner.Instance.OnEnemyDefeated -= IncreaseStreak;
 
+        /// <summary>
+        /// Increases the current streak by 1.
+        /// </summary>
         [ServerRpc(RequireOwnership = false)]
         public void IncreaseStreak()
         {
@@ -41,6 +31,9 @@ namespace Examen.Streak
             BroadCastStreak(_currentStreak);
         }
 
+        /// <summary>
+        /// Resets the current streak to 0.
+        /// </summary>
         [ServerRpc(RequireOwnership = false)]
         public void ResetStreak()
         {
@@ -48,6 +41,10 @@ namespace Examen.Streak
             BroadCastStreak(_currentStreak);
         }
 
+        /// <summary>
+        /// Broadcasts the current streak to all clients.
+        /// </summary>
+        /// <param name="streak">The current streak.</param>
         [ObserversRpc]
         private void BroadCastStreak(int streak)
         {
