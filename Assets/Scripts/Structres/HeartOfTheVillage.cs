e0765fd6dd5ebab8a9369bcfdddc4a45a20507d1
Minoord <70703952+Minoord@users.noreply.github.com>
2024-05-23 09:50:23 +0200

    Added HeartOfTheVillage.cs to a namespace

--- Assets/Scripts/Structres/HeartOfTheVillage.cs
+++ Assets/Scripts/Structres/HeartOfTheVillage.cs
@@ -2,70 +2,72 @@ using Examen.Networking;
 using Examen.Spawning.BossSpawning;
 using FishNet.Object;
 using MarkUlrich.Health;
+using System;
 using System.Collections;
 using UnityEngine;
 
-[RequireComponent(typeof(HealthData))]
-public class HeartOfTheVillage : NetworkBehaviour
+namespace Examen.Structures
 {
-    [SerializeField] private Material _crystalMaterial;
-    [SerializeField] private Color _damageColor;
-    [SerializeField] private float _regenTimer = 1f;
-
-    private Color _originalColor;
-    private HealthData _healthData;
-    private float _maxHealth;
-
-    private void Start()
+    [RequireComponent(typeof(HealthData))]
+    public class HeartOfTheVillage : NetworkBehaviour
     {
-        ServerInstance.Instance.OnServerStarted += InitialiseHeart;
-        _healthData = GetComponent<HealthData>();
-    }
+        [SerializeField] private Material _crystalMaterial;
+        [SerializeField] private Color _damageColor;
+        [SerializeField] private float _regenTimer = 1f;
 
-    private void InitialiseHeart()
-    {
-        _originalColor = _crystalMaterial.color;
-        _healthData.onDie.AddListener(ResetLevel);
-        _healthData.onDamageTaken.AddListener(GetDamaged);
+        private Color _originalColor;
+        private HealthData _healthData;
+        private float _maxHealth;
 
-        _maxHealth = _healthData.MaxHealth;
-    }
+        public Action OnResetLevel;
 
-    private void ResetLevel()
-    {
-        EnemySpawner.Instance.DespawnEnemies();
-        //TODO: reset streak to 0
-        StartCoroutine(Regenerate());
-    }
-
-    private IEnumerator Regenerate()
-    {
-        float time = 0f;
-        while (time < _regenTimer)
+        private void Start()
         {
-            time += Time.deltaTime;
-            _crystalMaterial.color = Color.Lerp(_damageColor, _originalColor, time / _regenTimer);
-            BroadcastNewColor(_crystalMaterial.color);
-            yield return null;
+            ServerInstance.Instance.OnServerStarted += InitialiseHeart;
+            _healthData = GetComponent<HealthData>();
         }
 
-        _healthData.Resurrect(_maxHealth);
+        private void InitialiseHeart()
+        {
+            _originalColor = _crystalMaterial.color;
+            _healthData.onDie.AddListener(ResetLevel);
+            _healthData.onDamageTaken.AddListener(GetDamaged);
+
+            _maxHealth = _healthData.MaxHealth;
+        }
+
+        private void ResetLevel()
+        {
+            EnemySpawner.Instance.DespawnEnemies();
+            OnResetLevel?.Invoke();
+            StartCoroutine(Regenerate());
+        }
+
+        private IEnumerator Regenerate()
+        {
+            float time = 0f;
+            while (time < _regenTimer)
+            {
+                time += Time.deltaTime;
+                _crystalMaterial.color = Color.Lerp(_damageColor, _originalColor, time / _regenTimer);
+                BroadcastNewColor(_crystalMaterial.color);
+                yield return null;
+            }
+
+            _healthData.Resurrect(_maxHealth);
+        }
+
+        private void GetDamaged()
+        {
+            float multiplier = 1 - _healthData.Health / _healthData.MaxHealth;
+            _crystalMaterial.color = Color.Lerp(_crystalMaterial.color, _damageColor, multiplier);
+            BroadcastNewColor(_crystalMaterial.color);
+        }
+
+        [ObserversRpc]
+        private void BroadcastNewColor(Color newColor) => _crystalMaterial.color = newColor;
+
+
+        private void OnDestroy() => _crystalMaterial.color = _originalColor;
     }
-
-    private void GetDamaged()
-    {
-        float multiplier = 1 - _healthData.Health / _healthData.MaxHealth;
-        _crystalMaterial.color = Color.Lerp(_crystalMaterial.color, _damageColor, multiplier);
-        BroadcastNewColor(_crystalMaterial.color);
-    }
-
-    [ObserversRpc]
-    private void BroadcastNewColor(Color newColor) => _crystalMaterial.color = newColor;
-
-
-    private void OnDestroy()
-    {
-        _crystalMaterial.color = _originalColor;
-    }
-
 }
